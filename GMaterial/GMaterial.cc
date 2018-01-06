6816b5b2a168753a64115b8b0fe3d81bee306c61
JW1992 <jiaweix@umich.edu>
2018-01-06 16:02:08 -0500

    CMakeLists added sqlite3 linker

--- GMaterial/GMaterial.cc
+++ GMaterial/GMaterial.cc
@@ -1,7 +1,22 @@
 #include "GMaterial.h"
+#include <stdio.h>
 
 bool GMaterial::SQLDataFetch(){
-	return false;
+	//return false;
+	sqlite3 *db;
+   	char *zErrMsg = 0;
+   	int rc;
+
+   	rc = sqlite3_open("NISTElementsGammaAttenuation.db", &db);
+
+   	if( rc ) {
+      	fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+      	return false;
+  	} else {
+      	fprintf(stderr, "Opened database successfully\n");
+   	}
+   	sqlite3_close(db);
+   	return true;
 }
 
 int main(){
