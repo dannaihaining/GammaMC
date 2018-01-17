c22a4fbb812774ec166d1a9374d72738a6e7a559
JW1992 <jiaweix@umich.edu>
2018-01-16 20:05:21 -0500

    Basic settings file format and parser

--- SimTestRun.cc
+++ SimTestRun.cc
@@ -11,6 +11,9 @@
 */
 #include <iostream>
 #include <fstream>
+#include <sstream>
+#include <string>
+#include <regex>
 
 
 //For now I just used the linear attenuation coefficient of CZT at 662 keV. (1/cm)
@@ -47,12 +50,80 @@ void GetAtten_All_1(double fE, double& fCS_C, double& fCS_P, double& fCS_E){
 }
 */
 
+bool ProcessConfig(GSimProcess* pGammaSim){
+	std::ifstream infile("../Settings.ini");//No need to release the memory manually this way
+	std::string line;
+	
+    std::regex rComment ("^#");
+    std::regex rBlank ("^\s*$");
+    std::regex rTime ("^SIM_TIME");
+    std::regex rPointSouce ("^SOURCE_POINT");
+    std::regex rCuboidObject ("^OBJ_CUBOID");
+    std::smatch m;
+    std::string strKeyWord;
+    
+    bool bIfInputValid = true;
+	
+	double fTime = 0.0;
+	
+	while (std::getline(infile, line))
+	{
+    	if(std::regex_search (line,m,rComment)) continue;
+    	if(std::regex_search (line,m,rBlank)) continue;
+    	//Input time length
+    	if(std::regex_search (line,m,rTime)){
+    		std::istringstream iss(line);
+    		if(!(iss >> strKeyWord)) bIfInputValid = false;
+    		if(!(iss >> fTime)) bIfInputValid = false;
+    		std::cout<<"Simulation time length: "<<fTime<<" seconds"<<std::endl;
+    	}
+    	//Input source type
+    	if(std::regex_search (line,m,rPointSouce)){
+    		std::istringstream iss(line);
+    		if(!(iss >> strKeyWord)) bIfInputValid = false;
+    		double x,y,z,E,fA;
+    		if(!(iss >> x >> y >> z >> E >> fA)) bIfInputValid = false;
+    		std::cout<<"Add point source, x="<<x<<", y="<<y<<", z="<<z;
+    		std::cout<<", Energy: " << E << " MeV" <<", Activity: "<<fA<<" uCi."<<std::endl;
+    		pGammaSim->AddNewSource(new GPointSource(x, y, z, E));
+    	}
+    	//Input object type
+    	if(std::regex_search (line,m,rCuboidObject)){
+    		std::istringstream iss(line);
+    		if(!(iss >> strKeyWord)) bIfInputValid = false;
+    		double x1,y1,z1,x2,y2,z2;
+    		std::string strMaterialName;
+    		int nIsDetector;
+    		if(!(iss >> x1 >> y1 >> z1 >> x2 >> y2 >> z2)) bIfInputValid = false;
+    		if(!(iss >> strMaterialName)) bIfInputValid = false;
+    		if(!(iss >> nIsDetector)) bIfInputValid = false;
+    		std::cout<<"Add new object: cuboid of "<<strMaterialName;
+    		if(nIsDetector>0) std::cout<<" as active detctor volume.";
+    		std::cout<<std::endl;
+    		std::cout<<"Position: "<<x1<<" "<<y1<<" "<<z1<<" "<<x2<<" "<<y2<<" "<<z2<<" "<<std::endl;
+    		pGammaSim->AddNewObject(new GCuboid(x1, y1, z1, x2, y2, z2, (nIsDetector>0)));
+    	}
+    	if(!bIfInputValid){
+    		std::cout<<"Input file exception"<<std::endl;
+    		return false;
+    	}
+	}
+	if(fTime<=0.0){
+		std::cout<< "Input time length invalid!" << std::endl;
+	}
+	std::cout<< "Simulation time length " << fTime << "seconds." << std::endl;
+	pGammaSim->PumpDecays(fTime);
+	return true;
+}
+
 int main(){
 	
 	GSimProcess* pGammaSim = new GSimProcess();
 	srand(time(0));  // Initialize random number generator.
-	std::cout << "Gamma sim start" << std::endl;
+	std::cout << "Configuring simulation settings ..." << std::endl;
 	
+	if(!ProcessConfig(pGammaSim)) return 0;
+	/*
   	// Load queue with some number of initial emissions.
   	double f_SourceE = 0.662;//Cs-137 source
   	//double f_SourceE = 0.183;//Uranium line
@@ -67,6 +138,8 @@ int main(){
   	std::cout << "Starting to pump queue with events"<< std::endl;
   	
   	pGammaSim->PumpDecays(3E7);
+  	
+  	*/
   	
   	std::cout << "Start simulation" << std::endl;
   	// Run the simulation.
